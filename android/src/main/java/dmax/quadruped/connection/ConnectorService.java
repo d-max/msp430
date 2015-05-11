d23385fcffd623e927edf5e2d5acabcbd8ffd887
Maxim Dybarskiy <maxim.dybarskyy@gmail.com>
2015-05-11 18:01:46 +0200

    Service draft implementation

--- android/src/main/java/dmax/quadruped/connection/ConnectorService.java
+++ android/src/main/java/dmax/quadruped/connection/ConnectorService.java
@@ -2,7 +2,14 @@ package dmax.quadruped.connection;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by Maksym Dybarskyi | maxim.dybarskyy@gmail.com
@@ -10,8 +17,76 @@ import android.os.IBinder;
  */
 public class ConnectorService extends Service {
 
+    static final int COMMAND = 0x45;
+
+    private Messenger localMessenger;
+    private Handler commandProcessor;
+    private ExecutorService worker;
+    private Connector connector;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        connector = null; // todo init
+        commandProcessor = new CommandProcessor();
+        localMessenger = new Messenger(commandProcessor);
+
+        worker = Executors.newSingleThreadExecutor();
+        worker.execute(new Runnable() {
+            @Override
+            public void run() {
+                connector.connect();
+            }
+        });
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return localMessenger.getBinder();
+    }
+
+    @Override
+    public void onDestroy() {
+        worker.execute(new Runnable() {
+            @Override
+            public void run() {
+                connector.disconnect();
+            }
+        });
+        worker.shutdown();
+    }
+
+    //~
+
+    private class CommandProcessor extends Handler {
+
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what != COMMAND) {
+                return;
+            }
+            final Messenger replyTo = msg.replyTo;
+            final int servoId = msg.arg1;
+            final int servoAngle = msg.arg2;
+
+            worker.execute(new Runnable() {
+                @Override
+                public void run() {
+                    boolean result = connector.send(servoId, servoAngle);
+                    sendResult(result, replyTo);
+                }
+            });
+        }
+
+        private void sendResult(boolean result, Messenger replyTo) {
+            try {
+                Message reply = Message.obtain();
+                reply.obj = result;
+                replyTo.send(reply);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
---
+++ android/src/main/java/dmax/quadruped/connection/ConnectorServiceClient.java
@@ -0,0 +1,8 @@
+package dmax.quadruped.connection;
+
+/**
+ * Created by Maksym Dybarskyi | maxim.dybarskyy@gmail.com
+ * on 11.05.15 at 17:22
+ */
+public class ConnectorServiceClient {
+}
