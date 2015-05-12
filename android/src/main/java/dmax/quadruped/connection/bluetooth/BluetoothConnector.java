cfe3b7f9241cd06b3eb3938c235cd848a7b7b420
Maxim Dybarskiy <maxim.dybarskyy@gmail.com>
2015-05-12 14:05:47 +0200

    Reimplement Bluetooth connector

--- android/src/main/java/dmax/quadruped/connection/bluetooth/BluetoothConnector.java
+++ android/src/main/java/dmax/quadruped/connection/bluetooth/BluetoothConnector.java
@@ -7,15 +7,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.*;
-import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Semaphore;
 
 import dmax.quadruped.Util;
+import dmax.quadruped.connection.Connector;
 
 import static android.bluetooth.BluetoothAdapter.*;
 
@@ -23,173 +22,80 @@ import static android.bluetooth.BluetoothAdapter.*;
  * Created by Maxim Dybarsky | maxim.dybarskyy@gmail.com
  * on 16.04.15 at 16:11
  */
-public class BluetoothConnector implements Constants {
+public class BluetoothConnector implements Connector, Constants {
 
-    private static final String TAG = "Quadruped";
-
-    private CountDownLatch lock = new CountDownLatch(1);
+    private Semaphore bluetoothEnableLock = new Semaphore(0);
+    private BroadcastReceiver receiver;
     private Context context;
-    private HandlerThread worker;
-    private Handler btHandler;
-    private Handler uiHandler;
+    private BluetoothAdapter adapter;
+    private BluetoothSocket socket;
+    private OutputStream out;
+    private InputStream in;
 
-    public BluetoothConnector(Context context, BluetoothCallback callback) {
+    public BluetoothConnector(Context context) {
         this.context = context;
-        this.uiHandler = new Handler(new CallbackInvoker(callback));
-        this.worker = new BluetoothWorker();
-        this.worker.start();
     }
 
     public void connect() {
-        getBtHandler().obtainMessage(MESSAGE_CONNECT).sendToTarget();
-    }
+        if (adapter == null) adapter = BluetoothAdapter.getDefaultAdapter();
 
-    public void disconnect() {
-        getBtHandler().obtainMessage(MESSAGE_DISCONNECT).sendToTarget();
-    }
-
-    public void send(BluetoothMessage message) {
-        getBtHandler().obtainMessage(MESSAGE_COMMAND, message.servoId, message.angle).sendToTarget();
-    }
-
-    public void quit() {
-        this.worker.quit();
-    }
-
-    private Handler getBtHandler() {
+        if (adapter.isEnabled()) {
+            onReady();
+        } else {
+            receiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    if (intent.getIntExtra(EXTRA_STATE, -1) == STATE_ON) onReady();
+                }
+            };
+            context.registerReceiver(receiver, new IntentFilter(ACTION_STATE_CHANGED));
+            adapter.enable();
+        }
         try {
-            lock.await();
+            bluetoothEnableLock.acquire();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return btHandler;
     }
 
-    //~
-
-    private class CallbackInvoker implements Handler.Callback {
-
-        private BluetoothCallback callback;
-
-        public CallbackInvoker(BluetoothCallback callback) {
-            this.callback = callback;
-        }
-
-        @Override
-        public boolean handleMessage(android.os.Message msg) {
-            switch (msg.what) {
-                case CALLBACK_CONNECTED:
-                    callback.onConnected();
-                    return true;
-                case CALLBACK_FAILED:
-                    callback.onFailed();
-                    return true;
-                case CALLBACK_RESPONSE:
-                    callback.onResponse(BluetoothCallback.Response.valueOf(msg.arg1));
-                    return true;
-            }
-            return false;
-        }
-    }
-
-    private class MessagesProcessor implements Handler.Callback {
-
-        private BroadcastReceiver receiver;
-
-        private BluetoothAdapter adapter;
-        private BluetoothSocket socket;
-        private OutputStream out;
-        private InputStream in;
-
-        @Override
-        public boolean handleMessage(android.os.Message msg) {
-            switch (msg.what) {
-                case MESSAGE_CONNECT:
-                    onConnect();
-                    return true;
-                case MESSAGE_DISCONNECT:
-                    onDisconnect();
-                    return true;
-                case MESSAGE_COMMAND:
-                    onCommand(msg.arg1, msg.arg2);
-                    return true;
-                default: return false;
-            }
-        }
-
-        private void onConnect() {
-            if (adapter == null) adapter = BluetoothAdapter.getDefaultAdapter();
-
-            if (adapter.isEnabled()) {
-                onReady();
-            } else {
-                receiver = new BroadcastReceiver() {
-                    @Override
-                    public void onReceive(Context context, Intent intent) {
-                        if (intent.getIntExtra(EXTRA_STATE, -1) == STATE_ON) onReady();
-                    }
-                };
-                context.registerReceiver(receiver, new IntentFilter(ACTION_STATE_CHANGED));
-                adapter.enable();
-            }
-        }
-
-        private void onReady() {
-            Log.i(TAG, "Connect to bluetooth dongle");
-            try {
-                BluetoothDevice device = adapter.getRemoteDevice(ADDRESS);
-                socket = device.createRfcommSocketToServiceRecord(java.util.UUID.fromString(UUID));
-                socket.connect();
-                out = socket.getOutputStream();
-                in = socket.getInputStream();
-                uiHandler.obtainMessage(CALLBACK_CONNECTED).sendToTarget();
-            } catch (IOException e) {
-                e.printStackTrace();
-                Util.close(in);
-                Util.close(out);
-                Util.close(socket);
-                uiHandler.obtainMessage(CALLBACK_FAILED).sendToTarget();
-            }
-        }
-
-        private void onDisconnect() {
-            Log.i(TAG, "Disconnect from bluetooth dongle");
-
-            if (receiver != null) context.unregisterReceiver(receiver);
-
+    public boolean send(int servoId, int angle) {
+        int response = RESPONSE_FAILED;
+        try {
+            String message = String.format(MESSAGE_TEMPLATE, servoId, angle);
+            out.write(message.getBytes());
+            out.flush();
+            response = in.read();
+        } catch (IOException ex) {
+            ex.printStackTrace();
             Util.close(in);
             Util.close(out);
             Util.close(socket);
         }
+        return response == RESPONSE_OK;
+    }
 
-        private void onCommand(int servoId, int angle) {
-            String message = String.format("S%02dA%03d\n", servoId, angle);
-            Log.i(TAG, "Send command: " + message);
-            try {
-                out.write(message.getBytes());
-                out.flush();
-                int response = in.read();
-                uiHandler.obtainMessage(CALLBACK_RESPONSE, response).sendToTarget();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-                Util.close(in);
-                Util.close(out);
-                Util.close(socket);
-                uiHandler.obtainMessage(CALLBACK_FAILED).sendToTarget();
-            }
+    private void onReady() {
+        try {
+            BluetoothDevice device = adapter.getRemoteDevice(ADDRESS);
+            socket = device.createRfcommSocketToServiceRecord(java.util.UUID.fromString(UUID));
+            socket.connect();
+            out = socket.getOutputStream();
+            in = socket.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Util.close(in);
+            Util.close(out);
+            Util.close(socket);
+        } finally {
+            bluetoothEnableLock.release();
         }
     }
 
-    private class BluetoothWorker extends HandlerThread {
+    public void disconnect() {
+        if (receiver != null) context.unregisterReceiver(receiver);
 
-        public BluetoothWorker() {
-            super("bt", android.os.Process.THREAD_PRIORITY_BACKGROUND);
-        }
-
-        @Override
-        protected void onLooperPrepared() {
-            btHandler = new Handler(getLooper(), new MessagesProcessor());
-            lock.countDown();
-        }
+        Util.close(in);
+        Util.close(out);
+        Util.close(socket);
     }
 }
--- android/src/main/java/dmax/quadruped/connection/bluetooth/Constants.java
+++ android/src/main/java/dmax/quadruped/connection/bluetooth/Constants.java
@@ -8,12 +8,8 @@ interface Constants {
 
     String ADDRESS = "00:12:06:21:88:70";
     String UUID = "00001101-0000-1000-8000-00805F9B34FB";
+    String MESSAGE_TEMPLATE = "S%02dA%03d\n";
 
-    int MESSAGE_CONNECT = 0x01;
-    int MESSAGE_DISCONNECT = 0x02;
-    int MESSAGE_COMMAND = 0x03;
-
-    int CALLBACK_CONNECTED = 0x07;
-    int CALLBACK_FAILED = 0x08;
-    int CALLBACK_RESPONSE = 0x09;
+    int RESPONSE_OK = 0x01;
+    int RESPONSE_FAILED = 0x02;
 }
