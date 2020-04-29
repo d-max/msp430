48e041c6a90e0f734aa01a72ee0935179d3085f5
Maksym Dybarskyi <maksym.dybarskyi@fromatob.com>
2020-04-29 23:13:20 +0200

    TimerA driven servo PWM

--- native/src/main.c
+++ native/src/main.c
@@ -1,7 +1,7 @@
 #include <msp430g2553.h>
 #include "servo.h"
-#include "uart.h"
-#include "i2c.h"
+//#include "uart.h"
+//#include "i2c.h"
 
 void configure() {
     // turn off watchdog
@@ -22,23 +22,27 @@ int main(void) {
     // config periphery
     // configure_uart();
     // configure_servos();
-    configure_i2c();
+    // configure_i2c();
 
     // for test purpose
-    i2c_reset();
-    i2c_start_transmition();
+    // i2c_reset();
+    // i2c_start_transmition();
+
+    // testing PWM servo
+    configure_servos();
+    set_servo_angle(0, 180);
 
     // low power mode
     _BIS_SR(LPM0_bits);
 }
 
-#pragma vector = USCIAB0RX_VECTOR
-__interrupt void usci_rx_isr(void) {
-    uart_data_received();
-}
+// #pragma vector = USCIAB0RX_VECTOR
+// __interrupt void usci_rx_isr(void) {
+//     uart_data_received();
+// }
 
-#pragma vector = USCIAB0TX_VECTOR
-__interrupt void usci_tx_isr(void) {
-    // uart_data_send();
-    i2c_data_send();
-}
+// #pragma vector = USCIAB0TX_VECTOR
+// __interrupt void usci_tx_isr(void) {
+//     // uart_data_send();
+//     i2c_data_send();
+// }
--- native/src/servo.h
+++ native/src/servo.h
@@ -6,4 +6,4 @@ void configure_servos();
 
 void set_servo_angle(unsigned char servo_id, unsigned char angle);
 
-int check_data_range(unsigned char servo_id, unsigned char angle);
+// int check_data_range(unsigned char servo_id, unsigned char angle);
