c7eb0a29caf616801420dc15daff8bc876c1bf73
Maksym Dybarskyi <maksym.dybarskyi@fromatob.com>
2020-05-01 23:05:17 +0200

    uart test

--- native/src/main.c
+++ native/src/main.c
@@ -1,48 +1,52 @@
-#include <msp430g2553.h>
-#include "servo.h"
-//#include "uart.h"
-//#include "i2c.h"
-
-void configure() {
-    // turn off watchdog
-    WDTCTL = WDTPW + WDTHOLD;
-
-    // use frequency 12 MHz
-    BCSCTL1 = CALBC1_12MHZ;
-    DCOCTL = CALDCO_12MHZ;
-
-    P1OUT = 0;
-    P2OUT = 0;
+#include "msp430g2553.h"
+ 
+#define TXLED BIT0
+#define RXLED BIT6
+#define TXD BIT2
+#define RXD BIT1
+ 
+const char string[] = { "Hello World\r\n" };
+unsigned int i; //Counter
+ 
+int main(void)
+{
+   WDTCTL = WDTPW + WDTHOLD; // Stop WDT
+   DCOCTL = 0; // Select lowest DCOx and MODx settings
+   BCSCTL1 = CALBC1_1MHZ; // Set DCO
+   DCOCTL = CALDCO_1MHZ;
+   P2DIR |= 0xFF; // All P2.x outputs
+   P2OUT &= 0x00; // All P2.x reset
+   P1SEL |= RXD + TXD ; // P1.1 = RXD, P1.2=TXD
+   P1SEL2 |= RXD + TXD ; // P1.1 = RXD, P1.2=TXD
+   P1DIR |= RXLED + TXLED;
+   P1OUT &= 0x00;
+   UCA0CTL1 |= UCSSEL_2; // SMCLK
+   UCA0BR0 = 0x08; // 1MHz 115200
+   UCA0BR1 = 0x00; // 1MHz 115200
+   UCA0MCTL = UCBRS2 + UCBRS0; // Modulation UCBRSx = 5
+   UCA0CTL1 &= ~UCSWRST; // **Initialize USCI state machine**
+   UC0IE |= UCA0RXIE; // Enable USCI_A0 RX interrupt
+   __bis_SR_register(CPUOFF + GIE); // Enter LPM0 w/ int until Byte RXed
+   while (1) { }
 }
-
-int main(void) {
-    // initial mcu config
-    configure();
-
-    // config periphery
-    // configure_uart();
-    // configure_servos();
-    // configure_i2c();
-
-    // for test purpose
-    // i2c_reset();
-    // i2c_start_transmition();
-
-    // testing PWM servo
-    configure_servos();
-    set_servo_angle(0, 180);
-
-    // low power mode
-    _BIS_SR(LPM0_bits);
+ 
+#pragma vector=USCIAB0TX_VECTOR
+__interrupt void USCI0TX_ISR(void) {
+    P1OUT |= TXLED; 
+    UCA0TXBUF = string[i++]; // TX next character 
+    if (i == sizeof string - 1) // TX over? 
+       UC0IE &= ~UCA0TXIE; // Disable USCI_A0 TX interrupt 
+    P1OUT &= ~TXLED; 
+} 
+  
+#pragma vector=USCIAB0RX_VECTOR 
+__interrupt void USCI0RX_ISR(void) { 
+   P1OUT |= RXLED; 
+    if (UCA0RXBUF == 'a') // 'a' received?
+    { 
+       i = 0; 
+       UC0IE |= UCA0TXIE; // Enable USCI_A0 TX interrupt 
+      UCA0TXBUF = string[i++]; 
+    } 
+    P1OUT &= ~RXLED;
 }
-
-// #pragma vector = USCIAB0RX_VECTOR
-// __interrupt void usci_rx_isr(void) {
-//     uart_data_received();
-// }
-
-// #pragma vector = USCIAB0TX_VECTOR
-// __interrupt void usci_tx_isr(void) {
-//     // uart_data_send();
-//     i2c_data_send();
-// }
