0164a1ba36e0d53f5275e5fbc4e511b4b71e3876
Maksym Dybarskyi <maksym.dybarskyi@fromatob.com>
2020-05-02 19:01:55 +0200

    Add servo.cpp

--- native/src/Servo.cpp
+++ native/src/Servo.cpp
@@ -1,37 +1,38 @@
 /*
-min servo time = 500 us
-max servo time = 2400 us
-ticks per second = 4096 * 50 = 204800
-min ticks = 204800 / 1000000 * 500 = 103
-max ticks = 204800 / 1000000 * 2400 = 492
-one degree time = (492 - 103) / 180 = ~2
-*/
+ * This code controls servo angle using Pwm
+ *
+ * min servo time = 500 us
+ * max servo time = 2400 us
+ * ticks per second = 4096 * 50 = 204800
+ * min ticks = 204800 / 1000000 * 500 = 103
+ * max ticks = 204800 / 1000000 * 2400 = 492
+ * one degree time = (492 - 103) / 180 = ~2
+ */
 
-#include "Servo.hpp"
+#include "servo.h"
 
 #define MIN_TIME 103
 #define MAX_TIME 492
 #define ONE_DEGREE_TIME 2
 
-uint16_t angleToPwmValue(uint8_t angle) {
+uint16_t angle_to_pwm(uint8_t angle) {
     return MIN_TIME + ONE_DEGREE_TIME * angle;
 }
 
-Servo::Servo(uint8_t pin, PwmController *controller) {
-    this->pin = pin;
-    this->pwm = controller;
+Servo::Servo(uint8_t id, Pwm *pwm) {
+    this -> id = id;
+    this -> pwm = pwm;
 }
 
-uint8_t Servo::getAngle(void) {
-    return angle;
+void Servo::set_angle(uint8_t angle) {
+    uint16_t pwm_time = angle_to_pwm(angle);
+    pwm -> set_pwm(id, pwm_time);
 }
 
-void Servo::setAngle(uint8_t angle) {
-    this->angle = angle;
-    this->setPwm();
+void Servo::max_angle() {
+    pwm -> set_pwm(id, MAX_TIME);
 }
 
-void Servo::setPwm() {
-    uint16_t pwmValue = angleToPwmValue(this->angle);
-    pwm->setPwm(this->pin, pwmValue);
+void Servo::min_angle() {
+    pwm -> set_pwm(id, MIN_TIME);
 }
