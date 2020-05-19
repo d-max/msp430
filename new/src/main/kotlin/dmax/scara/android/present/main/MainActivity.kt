281236c10f2a48d701d5b4c9b3d0494752e90249
Maksym Dybarskyi <maksym.dybarskyi@fromatob.com>
2020-05-20 00:29:53 +0200

    Add Main activity

---
+++ new/src/main/kotlin/dmax/scara/android/present/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package dmax.scara.android.present.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dmax.scara.android.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
