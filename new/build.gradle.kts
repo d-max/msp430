04a00788b6f69031addfbde2a33575a262d955cf
Maksym Dybarskyi <maksym.dybarskyi@fromatob.com>
2020-05-21 22:30:05 +0200

    Add layouts

--- new/build.gradle.kts
+++ new/build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
 
     implementation("androidx.annotation:annotation:1.1.0")
     implementation("androidx.annotation:annotation-experimental:1.0.0")
-    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.fragment:fragment:1.2.4")
 
     implementation("androidx.constraintlayout:constraintlayout:2.0.0-beta6")
     implementation("androidx.viewpager2:viewpager2:1.1.0-alpha01")
