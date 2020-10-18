2db0c6e5ef8030236fbd1a295037e18ee28c6164
Maksym Dybarskyi <maksym.dybarskyi@fromatob.com>
2020-10-18 21:04:23 +0200

    Upgrade gradle, kotlin, plugin

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,27 +3,6 @@ plugins {
     kotlin("android")
 }
 
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.72")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.4")
-
-    implementation("androidx.annotation:annotation:1.1.0")
-    implementation("androidx.annotation:annotation-experimental:1.0.0")
-    implementation("androidx.fragment:fragment:1.2.4")
-
-    implementation("androidx.constraintlayout:constraintlayout:2.0.0-beta6")
-    implementation("androidx.viewpager2:viewpager2:1.1.0-alpha01")
-    implementation("androidx.cardview:cardview:1.0.0")
-
-    implementation("com.google.android.material:material:1.2.0-alpha06")
-
-    implementation("org.koin:koin-core:2.1.5")
-    implementation("org.koin:koin-core-ext:2.1.5")
-    implementation("org.koin:koin-android:2.1.5")
-    implementation("org.koin:koin-android-viewmodel:2.1.5")
-}
-
 android {
 
     compileSdkVersion(29)
@@ -44,4 +23,25 @@ android {
     }
 
     sourceSets["main"].java.srcDir("src/main/kotlin")
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.10")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+
+    implementation("androidx.annotation:annotation:1.1.0")
+    implementation("androidx.annotation:annotation-experimental:1.0.0")
+    implementation("androidx.fragment:fragment:1.2.5")
+
+    implementation("androidx.constraintlayout:constraintlayout:2.0.2")
+    implementation("androidx.viewpager2:viewpager2:1.1.0-alpha01")
+    implementation("androidx.cardview:cardview:1.0.0")
+
+    implementation("com.google.android.material:material:1.3.0-alpha03")
+
+    implementation("org.koin:koin-core:2.2.0-rc-3")
+    implementation("org.koin:koin-core-ext:2.2.0-rc-3")
+    implementation("org.koin:koin-android:2.2.0-rc-3")
+    implementation("org.koin:koin-android-viewmodel:2.2.0-rc-3")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.0.0")
-        classpath(kotlin("gradle-plugin", version = "1.3.72"))
+        classpath("com.android.tools.build:gradle:4.1.0")
+        classpath(kotlin("gradle-plugin", version = "1.4.10"))
     }
 }
 
