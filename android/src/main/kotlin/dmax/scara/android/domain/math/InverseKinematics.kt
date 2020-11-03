3bafdc9d811bebae3225bda8eb53baf60199726d
Maksym Dybarskyi <maksym.dybarskyi@fromatob.com>
2020-11-03 19:56:00 +0100

    Add calculation of position based on angles

--- android/src/main/kotlin/dmax/scara/android/domain/math/InverseKinematics.kt
+++ android/src/main/kotlin/dmax/scara/android/domain/math/InverseKinematics.kt
@@ -2,40 +2,88 @@ package dmax.scara.android.domain.math
 
 import kotlin.math.acos
 import kotlin.math.atan
+import kotlin.math.cos
 import kotlin.math.pow
+import kotlin.math.sin
 import kotlin.math.sqrt
 
-data class Condition<T : Number>(
-    val a: T,
-    val b: T,
-    val x: T,
-    val y: T
+data class PositionCondition(
+    val a: Int,
+    val b: Int,
+    val x: Int,
+    val y: Int
 )
 
-data class Result<T : Number>(
-    val alpha: T,
-    val beta: T
+data class AngleCondition(
+    val a: Int,
+    val b: Int,
+    val alpha: Int,
+    val beta: Int
 )
 
-fun calculateAngles(condition: Condition<Int>): Result<Int> {
+data class AngleResult(
+    val alpha: Int,
+    val beta: Int
+)
+
+data class PositionResult(
+    val x: Int,
+    val y: Int
+)
+
+fun calculateAngles(condition: PositionCondition): AngleResult {
     val (a, b, x, y) = condition.toDouble()
     val l = sqrt(x.pow(2) + y.pow(2))
     val alpha1 = atan(y / x).toAngles()
     val alpha2 = acos((l.pow(2) + a.pow(2) - b.pow(2)) / (2 * a * l)).toAngles()
     val beta = acos((a.pow(2) + b.pow(2) - l.pow(2)) / (2 * a * b)).toAngles()
-    return Result(alpha1 + alpha2, beta).toInt()
+    return angleResult(alpha1 + alpha2, beta)
 }
 
-private fun <T : Number> Condition<T>.toDouble() = Condition(
+fun calculateCoordinates(condition: AngleCondition): PositionResult {
+    val (a, b, alpha, beta) = condition.toDouble()
+    val l = sqrt(a.pow(2) + b.pow(2) - 2 * a * b * cos(beta))
+    val alpha2 = acos((l.pow(2) + a.pow(2) - b.pow(2)) / (2 * a * l)).toAngles()
+    val alpha1 = alpha - alpha2
+    val x = l * sin(alpha1)
+    val y = l * cos(alpha1)
+    return positionResult(x, y)
+}
+
+private data class DoubleTuple(
+    val one: Double,
+    val two: Double,
+    val three: Double,
+    val four: Double
+)
+
+private fun PositionCondition.toDouble() = DoubleTuple(
     a.toDouble(),
     b.toDouble(),
     x.toDouble(),
     y.toDouble()
 )
 
-private fun <T : Number> Result<T>.toInt() = Result(
+private fun AngleCondition.toDouble() = DoubleTuple(
+    a.toDouble(),
+    b.toDouble(),
+    alpha.toDouble(),
+    beta.toDouble()
+)
+
+private fun angleResult(alpha: Double, beta: Double) = AngleResult(
     alpha.toInt(),
     beta.toInt()
+)
+
+private fun positionResult(x: Double, y: Double) = PositionResult(
+    x.toInt(),
+    y.toInt()
+)
+
+private fun Pair<Double, Double>.toInt() = PositionResult(
+    first.toInt(),
+    second.toInt()
 )
 
 private fun Double.toAngles(): Double = this * 180 / 3.141592654
